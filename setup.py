765061b9b8a853b0c5e51884ad18030b74c38b2c
alexander kupyrev <alexandr.kupyrev@pladform.ru>
2020-08-11 18:56:46 +0300

    +

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(name='pathology',
+      version='0.1',
+      author='temp',
+      author_email='temp',
+      description='temp',
+      packages=find_packages(),
+      )
